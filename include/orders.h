8e4ef950b85e134185567d29907332f3c7e9cfc7
Aditya-1020 <adityareddy400@gamil.com>
2025-07-17 17:25:36 +0530

    basic order structure

--- include/orders.h
+++ include/orders.h
@@ -11,10 +11,17 @@
 #include <map>
 
 class OrderBook {
+    // Node: first is price and second is quantity
     std::map<double, int> bids;   // price -> quantity (decending)
     std::map<double, int> asks;   // price -> quantity (acending)
 
 public:
     void updateBid(double price, int quantity);
     void updateAsk(double price, int quantity);
+
+    // Helper
+    double getBestBid() const;
+    double getBestAsk() const;
+    int getBidQuantity(double price) const;
+    int getAskQuantity(double price) const;
 };
--- src/orders.cpp
+++ src/orders.cpp
@@ -1,2 +1,37 @@
 #include "orders.h"
 
+void OrderBook::updateBid(double price, int quantity) {
+    if (quantity > 0) {
+        bids[price] = quantity; // update price if not in map
+    } else {
+        bids.erase(price); // remove at this price level
+    }
+}
+
+void OrderBook::updateAsk(double price, int quantity) {
+    if (quantity > 0) {
+        asks[price] = quantity;
+    } else {
+        asks.erase(price);
+    }
+}
+
+double OrderBook::getBestBid() const {
+    if (bids.empty()) return 0.0;
+    return bids.rbegin()->first;
+}
+
+double OrderBook::getBestAsk() const {
+    if (asks.empty()) return 0.0;
+    return asks.begin()->first;
+}
+
+int OrderBook::getBidQuantity(double price) const {
+    auto op = bids.find(price);
+    return (op != bids.end()) ? op->second : 0;
+}
+
+int OrderBook::getAskQuantity(double price) const {
+    auto op = asks.find(price);
+    return (op != asks.end()) ? op->second : 0;
+}
